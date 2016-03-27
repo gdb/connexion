0a8d44daa44f7ad725553ba51fb3ad41b38c13ed
Greg Brockman <gdb@gregbrockman.com>
2016-03-27 15:47:58 -0700

    Make validation errors more palatable

--- connexion/decorators/validation.py
+++ connexion/decorators/validation.py
@@ -120,7 +120,11 @@ class RequestBodyValidator(object):
             validate(data, self.schema, format_checker=draft4_format_checker)
         except ValidationError as exception:
             logger.error("%s validation error: %s" % (flask.request.url, exception))
-            return problem(400, 'Bad Request', str(exception))
+            if data is None:
+                content_type = flask.request.headers['Content-Type']
+                if content_type != 'application/json':
+                    return problem(400, 'Bad Request', 'Failed to load request body. (HINT: your Content-Type is {}. Perhaps try Content-Type of application/json?)'.format(content_type))
+            return problem(400, 'Bad Request', '{} (while requesting {} {}). Diagnostic info: schema_path={}'.format(exception.message, flask.request.method, flask.request.path, '.'.join(exception.schema_path)))
 
         return None
 
